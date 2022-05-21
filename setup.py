a68086853cb4fe8d36029fbb747ed641b186c06e
zwolf21 <pbr112@naver.com>
2022-05-22 05:46:24 +0900

    verion

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['requests', 'requests-cache', 'bs4', 'fake-useragent', 'uncurl',
+    install_requires=['requests', 'requests-cache', 'bs4', 'fake-useragent', 'uncurl', 'requests-file',
     ],
 
     # List additional groups of dependencies here (e.g. development
