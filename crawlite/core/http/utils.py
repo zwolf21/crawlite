d343b29046fa959fec6f8b6f0e8b1e80e543dcbc
zwolf21 <pbr112@naver.com>
2022-07-12 14:31:37 +0900

    When the header is explicitly specified, the fake user agent does not work.
    Allows initialization of fake user agent through setting

--- crawlite/core/http/utils.py
+++ crawlite/core/http/utils.py
@@ -2,8 +2,8 @@ from fake_useragent import FakeUserAgent
 
 
 
-def set_user_agent(header, user_agent_name):
+def set_user_agent(header=None, user_agent_name='chrome', **fkagent_kwargs):
     header = header or {}
-    fagent = FakeUserAgent()
+    fagent = FakeUserAgent(**fkagent_kwargs)
     header['User-Agent'] = getattr(fagent, user_agent_name)
     return header
